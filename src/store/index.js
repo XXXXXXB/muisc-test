f463f6d9f5f60c06bbb2d4f9ed976248a39858a0
1376553905@qq.com <1376553905@qq.com>
2020-09-20 16:10:28 +0800

    登录模块

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import { getItem, setItem } from '@/utils/storage'
 Vue.use(Vuex)
-
+const USER_KEY = 'user_key'
 export default new Vuex.Store({
   state: {
+    user: getItem(USER_KEY) // 当前登录用户状态
   },
   mutations: {
+    setUser (state, data) {
+      state.user = data
+      setItem(USER_KEY, state.user)
+    }
   },
   actions: {
   },
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,29 +5,52 @@
       left-arrow
       @click-left="$router.back()"
     />
-    <van-cell-group>
+    <van-form
+      @submit="onLogin"
+      :show-error='false'
+      :show-error-message='false'
+      ref='login-form'
+    >
       <van-field
         v-model="user.mobile"
         left-icon="shouji"
         icon-prefix='iconfont'
         placeholder="请输入手机号"
+        :rules = 'formRules.mobile'
+        name='mobile'
       />
       <van-field
         v-model="user.code"
         clearable
-        left-icon="music-o"
+        icon-prefix='iconfont'
+        left-icon="yanzhengma"
         placeholder="请输入验证码"
+        :rules = 'formRules.code'
       >
         <template #button>
-          <van-button size="small" type="primary" round>发送验证码</van-button>
+          <van-count-down
+            :time='1000 * 60'
+            format='ss s'
+            v-if="isCountDownShow"
+            @finish="isCountDownShow = false"
+          />
+          <van-button
+            v-else
+            size="small"
+            type="primary"
+            round
+            @click.prevent="onSendSms"
+            :loading='isSendSmsLoading'
+          >发送验证码</van-button>
         </template>
       </van-field>
-      <van-button type="info" block @click="onLogin">登录</van-button>
-    </van-cell-group>
+      <van-button type="info" block>登录</van-button>
+    </van-form>
   </div>
 </template>
 <script>
-import { login } from '@/api/user'
+import { login, sendSms } from '@/api/user'
+import { Toast } from 'vant'
 export default {
   name: 'login',
   data () {
@@ -35,17 +58,47 @@ export default {
       user: {
         mobile: '',
         code: ''
-      }
+      },
+      formRules: {
+        mobile: [
+          { required: true, message: '请输入手机号' },
+          { pattern: /^1[3|5|7|8|9]\d{9}$/, message: '手机号格式错误' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码' },
+          { pattern: /^\d{6}$/, message: '验证码格式错误' }
+        ]
+      },
+      isCountDownShow: false,
+      isSendSmsLoading: false
     }
   },
   methods: {
     async onLogin () {
+      Toast.loading({
+        message: '登录中...',
+        forbidClick: true,
+        duration: 0
+      })
       try {
-        const res = await login(this.user)
-        console.log(res)
+        const { data } = await login(this.user)
+        Toast.success('登录成功')
+        this.$store.commit('setUser', data.data)
+      } catch (err) {
+        console.log(err)
+        Toast.fail('登录失败')
+      }
+    },
+    async onSendSms () {
+      try {
+        await this.$refs['login-form'].validate('mobile')
+        this.isSendSmsLoading = true
+        await sendSms(this.user.mobile)
+        this.isCountDownShow = true
       } catch (err) {
         console.log(err)
       }
+      this.isSendSmsLoading = false
     }
   }
 }
